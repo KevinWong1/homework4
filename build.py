366974b587aca88f682ee88604092812ce0c4624
k w <kevinwng0@gmail.com>
2020-04-04 14:43:37 -0700

    finish phase 3

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import glob
 import os
 from jinja2 import Template
 
-y = datetime.datetime.now().strftime('%Y')
+year = datetime.datetime.now().strftime('%Y')
 
 def main():
     pages = []
@@ -16,9 +16,9 @@ def main():
         pages.append({
             "filename": "content/" + file_name,
             "title": name_only,
-            "output": "docs/" + file_name
+            "output": file_name
         })
-
+# Create final pages using content files with jinja2 templating
     for page in all_html_files:
         file_name = os.path.basename(page)
         name_only, extension = os.path.splitext(file_name)
@@ -28,58 +28,11 @@ def main():
         results = template.render(
             title=name_only,
             content=content_page,
+            year=str(year),
+            pages=pages,
+            is_ready=True
         )
+        open("docs/"+file_name,"w+").write(results)
 
-
-
-
-
-#     for page in pages:
-#         almost_finished_page = apply_template(page['filename'])
-#         almost_almost_finished_page = almost_finished_page.replace('{{year}}', str(y))
-#         finished_page = almost_almost_finished_page.replace("{{title}}", page['title'])
-
-#         if (page['title'] == 'Blog'):
-#             x = finished_page.replace('{{blog_nav_class}}', 'active')
-#         elif(page['title'] == 'About Me'):
-#             x = finished_page.replace('{{about_nav_class}}', 'active')
-#         else:
-#             x = finished_page.replace('{{project_nav_class}}', 'active')
-
-#         output_file(page['output'], x)
-
-#     blog_posts = [
-#         {
-#         "filename": "blog/1.html",
-#         "date": "September 3rd, 2018",
-#         "title": "My experience so far as a noob at Kickstart Coding",
-#         },
-#         {
-#         "filename": "blog/2.html",
-#         "date": "September 10th, 2018",
-#         "title": "So far I really like Ubuntu Linux",
-#         },
-#         {
-#         "filename": "blog/3.html",
-#         "date": "September 15th, 2018",
-#         "title": "My thoughts on Python so far",
-#         },
-#         ]
-#     for blog in blog_posts:
-#         template = open("./templates/base.html").read()
-#         finished_page = template.replace("{{content}}", blog['title'])
-#         output_file(blog['filename'], finished_page)
-        
-# def apply_template(content_location):
-#     # Read in template
-#     template = open("./templates/base.html").read()
-#     # store "real content" to a variable
-#     real_content = (open(content_location).read())
-#     # string replacing
-#     finished_page = template.replace("{{content}}", real_content)
-#     return finished_page
-
-# def output_file(destination, page):
-#     open(destination, 'w+').write(page)
 
 main()
